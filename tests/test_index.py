2e20846e71372f94f31a2e7af76b5de43dd72327
Samuel Colvin <s@muelcolvin.com>
2023-03-10 17:43:34 +0100

    fix test

--- tests/test_index.py
+++ tests/test_index.py
@@ -5,12 +5,14 @@ from .conftest import Client
 
 @pytest.mark.parametrize('method', ['get', 'head'])
 def test_index(client: Client, method):
-    r = client.get('/')
+    r = client.request(method, '/')
     assert r.status_code == 200, r.text
     assert r.headers['content-type'] == 'text/html; charset=utf-8'
     if method == 'get':
         assert '<h1>Hooky</h1>' in r.text
         assert '<code>???</code>' in r.text
+    else:
+        assert r.text == ''
 
 
 @pytest.mark.parametrize('method', ['get', 'head'])
